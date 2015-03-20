f26793a5785736c78b88f2447da51bd39636ce39
Chris Wenger <chriswengermusic@gmail.com>
2015-03-20 15:32:05 -0400

    Redefine IncreasePiano function, still not recognized

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -1,6 +1,7 @@
 /**
  * Created by cwenger on 3/20/2015.
  */
+
 var increaseTempo = document.getElementById('plusTempo');
 increaseTempo.onclick = function() {
     var tempo = document.getElementById('showTempo').innerHTML;
@@ -20,4 +21,31 @@ decreaseDrums.onclick = function() {
     var volume = document.getElementById('dLevel').innerHTML;
     var newVolume = parseInt(volume) - 1;
     document.getElementById('dLevel').innerHTML = newVolume;
-}
+};
+
+var increaseDrums = document.getElementById("plusDrums");
+decreaseDrums.onclick = function() {
+    var volume = document.getElementById('dLevel').innerHTML;
+    var newVolume = parseInt(volume) + 1;
+    document.getElementById('dLevel').innerHTML = newVolume;
+};
+
+/*var increasePiano = document.getElementById("plusPiano");
+increasePiano.onclick = function() {
+    var volume = document.getElementById('pLevel').innerHTML;
+    var newVolume = parseInt(volume) + 1;
+    document.getElementById('pLevel').innerHTML = newVolume;
+};
+*/
+var decreasePiano = document.getElementById("minusPiano");
+decreasePiano.onclick = function() {
+    var volume = document.getElementById('pLevel').innerHTML;
+    var newVolume = parseInt(volume) - 1;
+    document.getElementById('pLevel').innerHTML = newVolume;
+};
+
+var increasePiano = function() {
+    var volume = parseInt(document.getElementById('pLevel').innerHTML);
+    var newVolume = volume + 1;
+    document.getElementsById('pLevel').innerHTML = newVolume;
+};
