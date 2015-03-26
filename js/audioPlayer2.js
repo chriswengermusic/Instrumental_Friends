3a241913090be4d6df01d28ed54a50abce1de75b
Chris Wenger <chriswengermusic@gmail.com>
2015-03-26 08:24:27 -0400

    Add transposition via solfege variables; reduce HTMLRequests

--- js/audioPlayer2.js
+++ js/audioPlayer2.js
@@ -16,53 +16,109 @@ var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
 var noteLength;      // length of "beep" (in seconds)
 //var canvas = document.createElement('canvas');
                     // the canvas element
-
+var cursorCanvas;
 //var canvasContext = cursor.getContext('2d');          // canvasContext is the canvas' context 2D
 var last16thNoteDrawn = -1; // the last "box" we drew on the screen
 var notesInQueue = [];      // the notes that have been put into the web audio,
                             // and may or may not have played yet. {note, time}
 var timerWorker = null;     // The Web Worker used to fire timer messages
-var Bb1,
-    B1,
-    C2,
-    Db2,
-    D2,
-    Eb2,
-    E2,
-    F2,
-    Gb2,
-    G2,
-    Ab2,
-    A2,
-    Bb2,
-    B2,
-    C3,
-    Db3,
-    D3,
-    Eb3,
-    E3,
-    F3,
-    Gb3,
-    G3,
-    Ab3,
-    A3,
-    Bb3,
-    B3,
-    C4,
-    Db4,
-    D4,
-    Eb4,
-    E4,
-    F4,
-    Gb4,
-    G4,
-    Ab4,
-    A4,
-    Bb4,
-    B4,
-    C5,
-    Db5,
-    D5,
+var piano = [
+    'audio/piano/mp3/Bb1.mp3',
+    'audio/piano/mp3/B1.mp3',
+    'audio/piano/mp3/C2.mp3',
+    'audio/piano/mp3/Db2.mp3',
+    'audio/piano/mp3/D2.mp3',
+    'audio/piano/mp3/Eb2.mp3',
+    'audio/piano/mp3/E2.mp3',
+    'audio/piano/mp3/F2.mp3',
+    'audio/piano/mp3/Gb2.mp3',
+    'audio/piano/mp3/G2.mp3',
+    'audio/piano/mp3/Ab2.mp3',
+    'audio/piano/mp3/A2.mp3',
+    'audio/piano/mp3/Bb2.mp3',
+    'audio/piano/mp3/B2.mp3',
+    'audio/piano/mp3/C3.mp3',
+    'audio/piano/mp3/Db3.mp3',
+    'audio/piano/mp3/D3.mp3',
+    'audio/piano/mp3/Eb3.mp3',
+    'audio/piano/mp3/E3.mp3',
+    'audio/piano/mp3/F3.mp3',
+    'audio/piano/mp3/Gb3.mp3',
+    'audio/piano/mp3/G3.mp3',
+    'audio/piano/mp3/Ab3.mp3',
+    'audio/piano/mp3/A3.mp3',
+    'audio/piano/mp3/Bb3.mp3',
+    'audio/piano/mp3/B3.mp3',
+    'audio/piano/mp3/C4.mp3',
+    'audio/piano/mp3/Db4.mp3',
+    'audio/piano/mp3/D4.mp3',
+    'audio/piano/mp3/Eb4.mp3',
+    'audio/piano/mp3/E4.mp3',
+    'audio/piano/mp3/F4.mp3',
+    'audio/piano/mp3/Gb4.mp3',
+    'audio/piano/mp3/G4.mp3',
+    'audio/piano/mp3/Ab4.mp3',
+    'audio/piano/mp3/A4.mp3',
+    'audio/piano/mp3/Bb4.mp3',
+    'audio/piano/mp3/B4.mp3',
+    'audio/piano/mp3/C5.mp3',
+    'audio/piano/mp3/Db5.mp3',
+    'audio/piano/mp3/D5.mp3'
+];
+var key = "C";
+var Do, Re, Mi, Fa, Sol, La, Ti;
+var stepNums = [0, 5, 7, 12, 14, 16, 17, 19, 21, 23, 24];
+var num;
+var oct;
+
+
+var pianotoLoad = [];
+function getPianoArray(){
+    switch(key) {
+        case "C":
+            num = 2;
+            break;
+        case "Db":
+            num = 3;
+            break;
+        case "D":
+            num = 4;
+            break;
+        case "Eb":
+            num = 5;
+            break;
+        case "E":
+            num = 6;
+            break;
+        case "F":
+            num = 7;
+            break;
+        case "Gb":
+            num = 8;
+            break;
+        case "G":
+            num = 9;
+            break;
+        case "Ab":
+            num = 10;
+            break;
+        case "A":
+            num = 11;
+            break;
+        case "Bb":
+            num = 0;
+            break;
+        case "B":
+            num = 1;
+            break;
+    }
+    for (var i=0; i < stepNums.length; i++){
+        pianotoLoad.push(piano[num + stepNums[i]]);
+    }
+}
+getPianoArray();
+console.log(pianotoLoad);
+var Do1, Fa1, Sol1, Do2, Re2, Mi2, Fa2, Sol2, La2, Ti2, Do3,
     kick,
     kickLow,
     snare,
@@ -208,23 +264,19 @@ decreasePiano.onclick = function() {
 function scheduleNote( beatNumber, time ) {
     // push the note on the queue, even if we're not playing.
     notesInQueue.push({note: beatNumber, time: time});
-    //if ((noteResolution == 1) && (beatNumber % 2))
-        //return; // we're not playing non-8th 16th notes
-    //if ((noteResolution == 2) && (beatNumber % 4))
-        //return; // we're not playing non-quarter 8th notes
+
     setDrumVolume();
     kickLevel = .5 * hipHopLevel;
     snareLevel = .5 * hipHopLevel;
     hatLevel = .2 * hipHopLevel;
 
-    function countOffWindow() {
-        if (beatNumber < preRoll) {
-            document.getElementById('countOff').style.display = 'inline-block';
-        }
-        else {
-            document.getElementById('countOff').style.display = 'none';
-        }
+    if (beatNumber < preRoll) {
+        document.getElementById('countOff').style.display = 'inline-block';
     }
+    else {
+        document.getElementById('countOff').style.display = 'none';
+    }
+
     // create an oscillator
     var osc = context.createOscillator();
     var gainNode = context.createGain ? context.createGain() : context.createGainNode();
@@ -305,14 +357,14 @@ function scheduleNote( beatNumber, time ) {
         }
     }
 //TODO: enable transposition via solfege
-    var I = [A4, Gb4, D4, D2];
-    var V = [A4, E4, Db4, A2];
-    var V7 = [A4, G4, E4, Db4, A2];
-    var IV = [B4, G4, D4, G2];
-    var vi = [B4, Gb4, D4, B2];
+    var I = [Do1, Sol1, Do2, Mi2, Sol2, Do3];
+    var V = [Sol1, Re2, Sol2, Ti2];
+    var V7 = [Sol1, Re2, Fa2, Sol2, Ti2];
+    var IV = [Fa1, Do2, Fa2, La2, Do3];
+    var vi = [Do1, Do2, Mi2, La2];
 
-    countOffWindow();
     accompaniment();
+    drawCursor5();
         function accompaniment() {
             if (beatNumber === 0 + preRoll) {
                 playChord(I, quarter, pianoLevel);
@@ -343,7 +395,7 @@ function scheduleNote( beatNumber, time ) {
             else if (beatNumber === 5 + preRoll) {
                 playSound(closedHat, quarter, hatLevel);
             }
-            else if (beatNumber === 6){
+            else if (beatNumber === 6 + preRoll){
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
             else if (beatNumber === 8 + preRoll) {
@@ -368,7 +420,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(kickLow, quarter, kickLevel);
                 playSound(closedHat, quarter, hatLevel);
             }
-            else if(beatNumber === 14) {
+            else if(beatNumber === 14 + preRoll) {
                 playSound(breakSnare, quarter, breakbeatLevel);
                 playSound(breakOHat, quarter, breakbeatLevel);
             }
@@ -380,7 +432,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(breakKick, quarter, breakbeatLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
-            else if (beatNumber === 18){
+            else if (beatNumber === 18 + preRoll){
                 playSound(breakKick, quarter, breakbeatLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
@@ -422,7 +474,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(kickLow, quarter, kickLevel);
                 playSound(closedHat, quarter, hatLevel);
             }
-            else if(beatNumber === 30){
+            else if(beatNumber === 30 + preRoll){
                 playSound(breakSnare, quarter, breakbeatLevel);
                 playSound(breakOHat, quarter, breakbeatLevel);
             }
@@ -434,7 +486,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(breakKick, quarter, breakbeatLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
-            else if (beatNumber === 34) {
+            else if (beatNumber === 34 + preRoll) {
                 playSound(breakKick, quarter, breakbeatLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
@@ -446,7 +498,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(breakSnare, quarter, breakbeatLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
-            else if (beatNumber === 38) {
+            else if (beatNumber === 38 + preRoll) {
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
             else if (beatNumber === 40 + preRoll) {
@@ -468,7 +520,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(snare, quarter, snareLevel);
                 playSound(breakCHat, quarter, breakbeatLevel);
             }
-            else if (beatNumber === 46) {
+            else if (beatNumber === 46 + preRoll) {
                 playSound(breakOHat, quarter, breakbeatLevel);
                 playSound(breakSnare, quarter, breakbeatLevel);
             }
@@ -549,7 +601,7 @@ function scheduleNote( beatNumber, time ) {
                 playSound(breakCHat, quarter, breakbeatLevel);
                 playSound(breakSnare, quarter, breakbeatLevel);
             }
-            else if (beatNumber === 62) {
+            else if (beatNumber === 62 + preRoll) {
                 playSound(breakSnare, quarter, breakbeatLevel);
                 playSound(breakOHat, quarter, breakbeatLevel);
             }
@@ -558,7 +610,6 @@ function scheduleNote( beatNumber, time ) {
             }
 
         }
-
 }
 
 function scheduler() {
@@ -587,11 +638,57 @@ function play() {
 
 function resetCanvas (e) {
     // resize the canvas - but remember - this clears the canvas too.
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    cursorCanvas.width = window.innerWidth;
+    cursorCanvas.height = window.innerHeight;
 
     //make sure we scroll to the top left.
     window.scrollTo(0,0);
+}
+var xPos;
+function getXVals() {
+    xPos = [];
+
+    for (var i = 0; i < noteData.length; i++) {
+        var x = 0;
+        var xDiff = 0;
+        var xIncr = 0;
+        var subdivision = [];
+        if (i == noteData.length - 1) {
+            xDiff = Math.floor(document.getElementById('viewer').width - (noteData[i].mX + noteData[i].noteX));
+        }
+        else {
+            xDiff = Math.floor((noteData[i+1].mX + noteData[i + 1].noteX) - (noteData[i].mX + noteData[i].noteX));
+        }
+        if (noteData[i].mX == 10 * scale * zoom || noteData[i].mX == 15 * scale * zoom) {
+            var xOffset = 55 * scale * zoom;
+        }
+        else {
+            xOffset = 30 * scale * zoom;
+        }
+        subdivision = 16 / parseInt(noteData[i].duration);
+        xIncr = Math.floor(xDiff / subdivision);
+        for (var j = 0; j < subdivision; j++) {
+            x = Math.floor(xOffset + (noteData[i].mX + noteData[i].noteX + (xIncr * j)));
+            xPos.push(x);
+        }
+    }
+}
+
+function drawCursor5() {
+    getXVals();
+    for (var f = 0; f < xPos.length; f++) {
+        var xVal = (xPos[f]).toString();
+        cursorCanvas = document.getElementById('positionMarker');
+        if(cursorCanvas) {
+            cursorCanvas.parentNode.removeChild(cursorCanvas);
+        }
+        var cursor = document.createElement('canvas');
+        cursor.id = 'positionMarker';
+        cursor.height = 85;
+        cursor.width = 4;
+        //document.getElementById('viewer').appendChild(cursor);
+        //document.getElementById('positionMarker').style.left = xVal + "px";
+
 }
 
 function draw() {
@@ -607,7 +704,7 @@ function draw() {
         // We only need to draw if the note has moved.
         if (last16thNoteDrawn != currentNote) {
             //var x = Math.floor(canvas.width * .9) / (16 * measureCount);
-            var cursorCanvas = document.getElementById('positionMarker');
+            cursorCanvas = document.getElementById('positionMarker');
             if(cursorCanvas) {
                 cursorCanvas.parentNode.removeChild(cursorCanvas);
             }
@@ -615,7 +712,7 @@ function draw() {
             cursor.id = 'positionMarker';
             cursor.height = 85;
             cursor.width = 4;
-            var xPos = [];
+            /*xPos = [];
 
                     for (var i = 0; i < noteData.length; i++) {
                         var x = 0;
@@ -635,20 +732,23 @@ function draw() {
                             xOffset = 30 * scale * zoom;
                         }
                         subdivision = 16 / parseInt(noteData[i].duration);
-                        xIncr = xDiff / subdivision;
+                        xIncr = Math.floor(xDiff / subdivision);
                             for (var j = 0; j < subdivision; j++) {
-                                x = xOffset + ( noteData[i].mX + noteData[i].noteX + (xIncr * j));
+                                x = Math.floor(xOffset + (noteData[i].mX + noteData[i].noteX + (xIncr * j)));
                                 xPos.push(x);
                             }
-                    /*for (var f = 0; f < xPos.length + preRoll; f++) {
-                        document.getElementById('viewer').appendChild(cursor);
-                        document.getElementById('positionMarker').style.left = (xPos + "px");
                     }*/
+
             }
+    /*for (var f = 0; f < xPos.length + preRoll; f++) {
+        var xVal = (xPos[f]).toString();
+        //document.getElementById('viewer').appendChild(cursor);
+        //document.getElementById('positionMarker').style.left = xVal + "px";
+        console.log(xVal);
             //for (var f = 0; f < xPos.length + preRoll; f++) {
                 //canvasContext.fillStyle = ( currentNote == f + preRoll) ? "#00FF00" : "white";
                 //canvasContext.fillRect(xPos[f], 0, 5, 100);
-            //}
+            //}*/
 
         last16thNoteDrawn = currentNote;
         }
@@ -698,8 +798,7 @@ function draw() {
 }
 console.log(getXValues());
 */
-var cursorTime = ((60 / tempo) / 128) * 1000;
-console.log(cursorTime);
+
 //setInterval(cursorDraw(){
 
 //}, (timePerTick));
@@ -783,47 +882,17 @@ console.log(cursorTime);
 
 
 function loadedPiano(PIANO) {
-    Bb1 = PIANO[0];
-    B1 = PIANO[1];
-    C2 = PIANO[2];
-    Db2 = PIANO[3];
-    D2 = PIANO[4];
-    Eb2 = PIANO[5];
-    E2 = PIANO[6];
-    F2 = PIANO[7];
-    Gb2 = PIANO[8];
-    G2 = PIANO[9];
-    Ab2 = PIANO[10];
-    A2 = PIANO[11];
-    Bb2 = PIANO[12];
-    B2 = PIANO[13];
-    C3 = PIANO[14];
-    Db3 = PIANO[15];
-    D3 = PIANO[16];
-    Eb3 = PIANO[17];
-    E3 = PIANO[18];
-    F3 = PIANO[19];
-    Gb3 = PIANO[20];
-    G3 = PIANO[21];
-    Ab3 = PIANO[22];
-    A3 = PIANO[23];
-    Bb3 = PIANO[24];
-    B3 = PIANO[25];
-    C4 = PIANO[26];
-    Db4 = PIANO[27];
-    D4 = PIANO[28];
-    Eb4 = PIANO[29];
-    E4 = PIANO[30];
-    F4 = PIANO[31];
-    Gb4 = PIANO[32];
-    G4 = PIANO[33];
-    Ab4 = PIANO[34];
-    A4 = PIANO[35];
-    Bb4 = PIANO[36];
-    B4 = PIANO[37];
-    C5 = PIANO[38];
-    Db5 = PIANO[39];
-    D5 = PIANO[40];
+    Do1 = PIANO[0];
+    Fa1 = PIANO[1];
+    Sol1 = PIANO[2];
+    Do2 = PIANO[3];
+    Re2 = PIANO[4];
+    Mi2 = PIANO[5];
+    Fa2 = PIANO[6];
+    Sol2 = PIANO[7];
+    La2 = PIANO[8];
+    Ti2 = PIANO[9];
+    Do3 = PIANO[10];
 }
 function loadedHipHop(HIPHOP) {
     kick = HIPHOP[0];
@@ -911,50 +980,7 @@ function init(){
     // if we wanted to load audio files, etc., this is where we should do it.
     var pianoLoader = new BufferLoader(
         context,
-        [
-            'audio/piano/mp3/Bb1.mp3',
-            'audio/piano/mp3/B1.mp3',
-            'audio/piano/mp3/C2.mp3',
-            'audio/piano/mp3/Db2.mp3',
-            'audio/piano/mp3/D2.mp3',
-            'audio/piano/mp3/Eb2.mp3',
-            'audio/piano/mp3/E2.mp3',
-            'audio/piano/mp3/F2.mp3',
-            'audio/piano/mp3/Gb2.mp3',
-            'audio/piano/mp3/G2.mp3',
-            'audio/piano/mp3/Ab2.mp3',
-            'audio/piano/mp3/A2.mp3',
-            'audio/piano/mp3/Bb2.mp3',
-            'audio/piano/mp3/B2.mp3',
-            'audio/piano/mp3/C3.mp3',
-            'audio/piano/mp3/Db3.mp3',
-            'audio/piano/mp3/D3.mp3',
-            'audio/piano/mp3/Eb3.mp3',
-            'audio/piano/mp3/E3.mp3',
-            'audio/piano/mp3/F3.mp3',
-            'audio/piano/mp3/Gb3.mp3',
-            'audio/piano/mp3/G3.mp3',
-            'audio/piano/mp3/Ab3.mp3',
-            'audio/piano/mp3/A3.mp3',
-            'audio/piano/mp3/Bb3.mp3',
-            'audio/piano/mp3/B3.mp3',
-            'audio/piano/mp3/C4.mp3',
-            'audio/piano/mp3/Db4.mp3',
-            'audio/piano/mp3/D4.mp3',
-            'audio/piano/mp3/Eb4.mp3',
-            'audio/piano/mp3/E4.mp3',
-            'audio/piano/mp3/F4.mp3',
-            'audio/piano/mp3/Gb4.mp3',
-            'audio/piano/mp3/G4.mp3',
-            'audio/piano/mp3/Ab4.mp3',
-            'audio/piano/mp3/A4.mp3',
-            'audio/piano/mp3/Bb4.mp3',
-            'audio/piano/mp3/B4.mp3',
-            'audio/piano/mp3/C5.mp3',
-            'audio/piano/mp3/Db5.mp3',
-            'audio/piano/mp3/D5.mp3'
-
-        ],
+        pianotoLoad,
         loadedPiano
     );
 
@@ -990,7 +1016,7 @@ function init(){
     window.onorientationchange = resetCanvas;
     window.onresize = resetCanvas;
 
-    requestAnimFrame(draw);    // start the drawing loop.
+    //requestAnimFrame(draw);    // start the drawing loop.
 
             timerWorker = new Worker("js/audioPlayerWorker.js");
 
